87a06eabea7d93e61fc4f1f7d636eae7d3ddf078
marksanghoonkim <mark.s.kim@gmail.com>
2016-03-19 11:00:41 -0700

    Implements ajax weather request, implements search on submit, sets up fetchWeather action

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,14 +1,50 @@
 import React, { Component  } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  };
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+
+    this.setState({ term: '' });
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form 
+        className="input-group" 
+        onSubmit={this.onFormSubmit.bind(this)} >
+        <input 
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange.bind(this)} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
       </form>
     );
-  }
+  };
 };
+
+// binds actioncreator fetchWeather to our SearchBar container
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// null is saying 'we dont need any state here'
+export default connect(null, mapDispatchToProps)(SearchBar);
